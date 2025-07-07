7df1ae2546782bc733747200eadb10662a0318d2
Faiz <ansarifaiz0905@gmail.com>
2025-07-08 01:43:34 +0530

    Add external links to social icons in Footer
    Replaced static social media icons with clickable links using React Router's Link component, allowing users to open WhatsApp, Facebook, YouTube, and GitHub in new tabs.

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,19 +1,48 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { FaWhatsapp } from "react-icons/fa";
 import { FaFacebook } from "react-icons/fa";
 import { FaYoutube } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 const Footer = () => {
   return (
-    <div className=" flex p-4 justify-between items-center">
+    <div className="flex p-4 justify-between items-center">
       <h1 className="text-[20px] font-black cursor-pointer">
         Made with ❤ by<span className="text-blue-600"> Faiz Ansari </span>
       </h1>
       <div className="flex text-[30px] gap-3">
-        <FaWhatsapp color="green" />
+        {/* <FaWhatsapp color="green" />
         <FaFacebook color="blue" />
         <FaYoutube color="red" />
-        <FaGithub color="black" />
+        <FaGithub color="black" /> */}
+        <Link
+          to="https://wa.me"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaWhatsapp color="green" />
+        </Link>
+        <Link
+          to="https://facebook.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaFacebook color="blue" />
+        </Link>
+        <Link
+          to="https://youtube.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaYoutube color="red" />
+        </Link>
+        <Link
+          to="https://github.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaGithub color="black" />
+        </Link>
       </div>
     </div>
   );
